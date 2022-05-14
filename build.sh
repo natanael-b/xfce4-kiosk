bd846acf211a3869ff0f1f8397cc19d1e3a26880
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2022-05-14 12:53:49 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+add-apt-repository ppa:xubuntu-dev/extras -y
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 sed -i 's|# deb-src|deb-src|g' /etc/apt/sources.list
 
 cat /etc/apt/sources.list
@@ -20,69 +24,12 @@ sed -i "${linha1},${linha2}s/gtk_widget_show (item);/\/\/ gtk_widget_show (item)
 
 linha1=$(grep -n "^xfce_panel_plugin_button_press_event (GtkWidget      \*widget,$" libxfce4panel/xfce-panel-plugin.c  | cut -d\: -f1)
 linha2=$(($(grep -n "gtk_menu_popup_at_pointer (menu, (GdkEvent \*) event);$" libxfce4panel/xfce-panel-plugin.c  | cut -d\: -f1)+1))
+
 sed -i "${linha1},${linha2}s/gtk_menu_popup_at_pointer (menu, (GdkEvent/\/\/ gtk_menu_popup_at_pointer (menu, (GdkEvent/" libxfce4panel/xfce-panel-plugin.c
 
 dpkg-buildpackage -b
 
 cd ..
-
-apt install ./*.deb  # Instalar os pacotes gerados é necessário para recompilar plugins que não vem por padrão
-
-plugins=("xfce4-docklike-plugin")
-baseurl="https://archive.xfce.org/src/panel-plugins/"
-
-echo 
-echo "---------------------------------------------------------------------------------------------"
-echo 
-
-debian_package=$(mktemp -d)
-
-for plugin in ${plugins[@]}; do
-  version=$(wget -q -O - "${baseurl}/${plugin}/"             | sed '$d' | sed '$d' | tail -n1 | cut -d\" -f2)
-  package=$(wget -q -O - "${baseurl}/${plugin}/${version}/"  | sed '$d' | sed '$d' | tail -n1 | cut -d\" -f2)
-  full_url="${baseurl}/${plugin}/${version}/${package}"
-  
-  echo 
-  echo " Plugin: ${plugin}"
-  echo "Version: ${version}"
-  echo "    URL: ${full_url}"
-  echo
-  wget "${full_url}"
-  echo
-  (
-    source_dir=$(basename -s.tar.bz2 "${package}")
-    
-    tar -xf "${package}"
-    
-    cd "${source_dir}"
-    
-    ./configure
-    make
-    make install | tee install-log
-    
-    grep "installing" install-log | cut -d' ' -f 4 | sed "s|^|cp --parents --verbose |g;s|$| ${debian_package}|g" | sh 
-    grep "libtool:"   install-log | cut -d' ' -f 6 | sed "s|^|cp --parents --verbose |g;s|$| ${debian_package}|g" | sh 
-    
-    cd..
-  )
-  echo
-  echo "---------------------------------------------------------------------------------------------"
-done
-
-mkdir -p "${debian_package}/DEBIAN"
-
-(
- echo "Package: xfce4-plugins-extra"
- echo "Priority: required"
- echo "Version: $(date +%y.%m.%d%H%M%S)"
- echo "Architecture: amd64"
- echo "Maintainer: Natanael Barbosa Santos"
- echo "Depends: xfce4-panel"
- echo "Description: Plugins extras pro XFCE4-Panel"
- echo
-) > ${debian_package}/DEBIAN/control
-
-dpkg -b ${debian_package} ./plugins.deb
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
 
